67ac24bca618b4c264ae2f2534c8cba7be4345a4
kcsheng <kacysheng@mail.com>
2021-06-06 00:08:01 +1000

    Add google font Montserrat

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
 // Write password to the #password input
 function writePassword() {
+  var generatePassword = function () {
+    return "Hi there";
+  };
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
